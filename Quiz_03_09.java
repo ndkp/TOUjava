c0ca3b93e31b051a78118718491aac191bd55f0c
ndkp <nk.we99@gmail.com>
2022-05-11 11:41:25 +0900

    new

---
+++ Quiz_03_09.java
@@ -0,0 +1,19 @@
+public class Quiz_03_09 {
+    public static void main(String[] args) {
+        int a = 8;
+
+        switch (a / 4.0) {
+            case 1:
+                System.out.println("case 1");
+                break;
+            case 2:
+                System.out.println("case 2");
+                break;
+            case 3:
+                System.out.println("case 3");
+                break;
+            default:
+                System.out.println("default");
+        }
+    }
+}
