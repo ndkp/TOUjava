e870e4e6bfdb36a4e71223a5c2f3fef910bfb8c7
ndkp <nk.we99@gmail.com>
2022-05-11 11:50:48 +0900

    new

---
+++ Quiz_03_03.java
@@ -0,0 +1,11 @@
+public class Quiz_03_03 {
+    public static void main(String[] args) {
+        int a = 5;
+
+        if (true) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+    }
+}
