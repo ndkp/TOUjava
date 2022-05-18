b1547b3be300498ec776822270d34ce5a223a84f
ndkp <nk.we99@gmail.com>
2022-05-19 00:35:11 +0900

    new

---
+++ Quiz_06_02.java
@@ -0,0 +1,7 @@
+public class Quiz_06_02 {
+    public static void main(String[] args) {
+            int[] a={100,200,300,400,500,600};
+
+            System.out.println(a.length);
+        }
+    }
