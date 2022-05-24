ee36846ba1ac265f2f2bea8fe191eb5261df1a20
ndkp <nk.we99@gmail.com>
2022-05-24 17:38:39 +0900

    new

---
+++ Quiz_05_08.java
@@ -0,0 +1,10 @@
+public class Quiz_05_08 {
+    public static void main(String[] args){
+        for (int i = 1; i <= 10; i++) {
+            if(i%3 == 0) continue;
+            if(i == 6) break;
+            System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+}
