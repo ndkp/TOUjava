ababa961338b656a78bfa7cae5f2f1f94059cadb
ndkp <nk.we99@gmail.com>
2022-11-15 18:08:31 +0900

    　

---
+++ 演Quiz_06_03.java
@@ -0,0 +1,10 @@
+public class 演Quiz_06_03 {
+    public static void main(String[] args) {
+
+        int array[] = new int[5];
+        array={10,20,30,40,50}; // エラー
+
+        int sum = array[1] + array[3];
+        System.out.println(sum);
+    }
+}
