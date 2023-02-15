6cac2063c8fbd48bf1948c2b5edf36f946cdce5f
ndkp <nk.we99@gmail.com>
2023-02-15 15:48:20 +0900

    演2Quiz_03

---
+++ 演2Quiz_03.java
@@ -0,0 +1,16 @@
+public class 演2Quiz_03 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        int j = 0;
+        while (j < 5) {
+            System.out.print((j + 1) + " ");
+            j++;
+        }
+    }
+}
